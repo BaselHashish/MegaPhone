3f13cd9fdc011e5aeaed3596e52ffde07eaf778e
BaselHashish <basel.hashish@gmail.com>
2015-12-08 22:18:00 +0200

    modification

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   root 'access#index'
-
   match ':controller(/:action(/:id))', :via => [:get, :post]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
